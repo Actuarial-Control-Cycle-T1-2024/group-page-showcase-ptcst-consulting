860e7c45a0a3815d7b44149ea406a07367d76e95
Tetian Madfouni <tmadfouni@gmail.com>
2024-03-21 11:14:51 +1100

    Adding sensitivity tests and tidying code

--- Code/code_for_submission.R
+++ Code/code_for_submission.R
@@ -9,6 +9,65 @@ library(foreach)
 # Set seed for randomness
 set.seed(1234)
 
+
+# PARAMETERS OF PROGRAMS
+intervention_progs = list(
+  safety_campaigns = list(effect_l = 0.03, effect_u = 0.04, cost_l = 30, cost_u = 35, horizon = 1, signup = 0.6, sub_rate = 1, effect_reduction = TRUE),
+  annual_checkup = list(effect_l = 0.05, effect_u = 0.07, cost_l = 300, cost_u = 870, horizon = 5, signup = 1, sub_rate = 1, effect_reduction = FALSE),
+  discount_gym = list(effect_l = 0.03, effect_u = 0.05, cost_l = 600, cost_u = 870, horizon = 1, signup = 0.2, sub_rate = 1, effect_reduction = TRUE),
+  weight_mgmt = list(effect_l = 0.05, effect_u = 0.065, cost_l = 500, cost_u = 870, horizon = 1, signup = 0.4, sub_rate = 1, effect_reduction = TRUE),
+  cancer_prevention = list(effect_l = 0.05, effect_u = 0.08, cost_l = 50, cost_u = 85, horizon = 9, signup = 0.5, sub_rate = 1, effect_reduction = TRUE),
+  heart_screenings = list(effect_l = 0.05, effect_u = 0.08, cost_l = 150, cost_u = 345, horizon = 5, signup = 0.5, sub_rate = 1, effect_reduction = FALSE)
+)
+
+mu_vec = c(
+  safety_campaigns = 0.04,
+  annual_checkup = 0.075,
+  discount_gym = 0.045,
+  weight_mgmt = 0.075,
+  cancer_prevention = 0.075,
+  heart_screenings = 0.075
+)
+
+find_var = function(lo, hi) {
+  ((hi - lo) / 6)^2
+}
+
+var_vec = c(
+  find_var(0.03, 0.04),
+  find_var(0.05, 0.07),
+  find_var(0.03, 0.05),
+  find_var(0.05, 0.065),
+  find_var(0.05, 0.08),
+  find_var(0.05, 0.08)
+)
+
+raw_covs = sqrt(outer(var_vec, var_vec, "*"))
+
+corr_mat = matrix(
+  data = c( 1,     -0.35,  0,     0,     -0.35,  0,
+            -0.35,  1,    -0.35,  0,     -0.25,  0,
+            0,     -0.35,  1,    -0.375, -0.25, -0.25,
+            0,      0,    -0.375, 1,     -0.3,  -0.1,
+            -0.35, -0.25, -0.25, -0.3,    1,    -0.1,
+            0,      0,    -0.25, -0.1,   -0.1,   1),
+  dimnames = list(names(intervention_progs), names(intervention_progs)),
+  nrow = 6,
+  ncol = 6
+)
+
+cov_mat = corr_mat * raw_covs
+
+
+
+# OTHER PARAMETERS
+
+# Proportion of people who will regularly meet health goals
+goal_meeting_rate = 0.7
+interest = 0.05
+
+
+
 # LOADING DATA
 # LIFE TABLE
 mort_table = readRDS("Data/ult_lt.RDS")
@@ -158,3 +217,5 @@ sim_func = function(intervention_progs, mort_data, data, interest) {
              exp_death_costs = death_costs,
              exp_death_costs_no_reduction = death_costs_no_red)
 }
+
+res = sim_func(intervention_progs, mort_table, full_data, interest)
--- Code/main_code_RUN_THIS.R
+++ Code/main_code_RUN_THIS.R
@@ -1,32 +1,21 @@
-# STILL REMAINING TO DO (but negligible)
-# ADD HARD LIMITS ON MULTIVARIATE NORMAL SIM
 
-# PARAMS
-# effect_u/effect_l/cost_u/cost_l lower and upper limits of cost and effects
-# horizon - how long it takes to take full effect
-# signup - proportion of people that will signup to this program
-# sub_rate - base subsidy rate (as of level 1)
-# effect_reduction - TRUE if the effect is only felt if someone puts effort in
-#                    i.e. heart checks help anyway, but gym memberships dont.
-
-set.seed(1234)
-
+# PARAMETERS OF PROGRAMS
 intervention_progs = list(
-  safety_campaigns = list(effect_l = 0.03, effect_u = 0.05, cost_l = 10, cost_u = 35, horizon = 1, signup = 0.7, sub_rate = 1, effect_reduction = TRUE),
-  annual_checkup = list(effect_l = 0.05, effect_u = 0.1, cost_l = 175, cost_u = 870, horizon = 5, signup = 1, sub_rate = 1, effect_reduction = FALSE),
-  discount_gym = list(effect_l = 0.03, effect_u = 0.06, cost_l = 175, cost_u = 870, horizon = 1, signup = 0.3, sub_rate = 1, effect_reduction = TRUE),
-  weight_mgmt = list(effect_l = 0.05, effect_u = 0.1, cost_l = 175, cost_u = 870, horizon = 1, signup = 0.6, sub_rate = 1, effect_reduction = TRUE),
-  cancer_prevention = list(effect_l = 0.05, effect_u = 0.1, cost_l = 20, cost_u = 85, horizon = 9, signup = 0.6, sub_rate = 1, effect_reduction = TRUE),
-  heart_screenings = list(effect_l = 0.05, effect_u = 0.1, cost_l = 90, cost_u = 345, horizon = 5, signup = 0.6, sub_rate = 1, effect_reduction = FALSE) 
+  safety_campaigns = list(effect_l = 0.03, effect_u = 0.04, cost_l = 30, cost_u = 35, horizon = 1, signup = 0.6, sub_rate = 1, effect_reduction = TRUE),
+  annual_checkup = list(effect_l = 0.05, effect_u = 0.07, cost_l = 300, cost_u = 870, horizon = 5, signup = 1, sub_rate = 1, effect_reduction = FALSE),
+  discount_gym = list(effect_l = 0.03, effect_u = 0.05, cost_l = 600, cost_u = 870, horizon = 1, signup = 0.2, sub_rate = 1, effect_reduction = TRUE),
+  weight_mgmt = list(effect_l = 0.05, effect_u = 0.065, cost_l = 500, cost_u = 870, horizon = 1, signup = 0.4, sub_rate = 1, effect_reduction = TRUE),
+  cancer_prevention = list(effect_l = 0.05, effect_u = 0.08, cost_l = 50, cost_u = 85, horizon = 9, signup = 0.5, sub_rate = 1, effect_reduction = TRUE),
+  heart_screenings = list(effect_l = 0.05, effect_u = 0.08, cost_l = 150, cost_u = 345, horizon = 5, signup = 0.5, sub_rate = 1, effect_reduction = FALSE)
 )
 
 mu_vec = c(
-  safety_campaigns = 0.03,
-  annual_checkup = 0.04,
-  discount_gym = 0.03,
-  weight_mgmt = 0.04,
-  cancer_prevention = 0.05,
-  heart_screenings = 0.05
+  safety_campaigns = 0.04,
+  annual_checkup = 0.075,
+  discount_gym = 0.045,
+  weight_mgmt = 0.075,
+  cancer_prevention = 0.075,
+  heart_screenings = 0.075
 )
 
 find_var = function(lo, hi) {
@@ -34,43 +23,37 @@ find_var = function(lo, hi) {
 }
 
 var_vec = c(
+  find_var(0.03, 0.04),
+  find_var(0.05, 0.07),
   find_var(0.03, 0.05),
-  find_var(0.05, 0.1),
-  find_var(0.03, 0.06),
-  find_var(0.05, 0.1),
-  find_var(0.05, 0.1),
-  find_var(0.05, 0.1)
+  find_var(0.05, 0.065),
+  find_var(0.05, 0.08),
+  find_var(0.05, 0.08)
 )
 
 raw_covs = sqrt(outer(var_vec, var_vec, "*"))
 
 corr_mat = matrix(
-  data = c( 1,    -0.35,  0,     0,     -0.35,  0,
-           -0.35,  1,    -0.35,  0,     -0.25,  0,
-            0,    -0.35,  1,    -0.375, -0.25, -0.25,
-            0,     0,    -0.375,   1,   -0.3,  -0.1,
-           -0.35, -0.25, -0.25, -0.3,    1,    -0.1,
-            0,     0,    -0.25, -0.1,   -0.1,   1),
+  data = c( 1,     -0.35,  0,     0,     -0.35,  0,
+            -0.35,  1,    -0.35,  0,     -0.25,  0,
+            0,     -0.35,  1,    -0.375, -0.25, -0.25,
+            0,      0,    -0.375, 1,     -0.3,  -0.1,
+            -0.35, -0.25, -0.25, -0.3,    1,    -0.1,
+            0,      0,    -0.25, -0.1,   -0.1,   1),
   dimnames = list(names(intervention_progs), names(intervention_progs)),
   nrow = 6,
   ncol = 6
 )
 
-
-
-# Cov Matrix. Fill out.
 cov_mat = corr_mat * raw_covs
 
-# How much the subsidy would increase by if they were at level 5
-subsidy_increase_max = 0.1
+
+
+# OTHER PARAMETERS
 
 # Proportion of people who will regularly meet health goals
 goal_meeting_rate = 0.7
-
-# Max premium discount for people who are at level 5
-premium_disc = 0.1
-
-interest = 0.03
+interest = 0.05
 
 source("Code/supplementary_to_main.R")
 
